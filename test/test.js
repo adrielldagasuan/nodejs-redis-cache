570cb662e9f2c8b8a36a0f915ae83f3589cc4936
ghparagas <gilwen.h.paragas@gmail.com>
2016-11-09 16:06:16 +0800

    fixed test case

--- test/test.js
+++ test/test.js
@@ -1,13 +1,12 @@
 // Simple async test for HTTP 200 response code using supertest
 'use strict';
 
-var request = require("supertest"),
-    app = require("../server").getApp;
+var request = require("supertest");
 
 describe('GET /api/redis/search/anything', function(){
-//  it("returns status 200", function() {
-//    request('/api/redis/search/anything', function(error, response, body) {
-//      expect(response.statusCode).to.equal(200);
-//    });
-//  });
+  it("returns status 200", function() {
+    request('/api/redis/search/anything', function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+    });
+  });
 });
